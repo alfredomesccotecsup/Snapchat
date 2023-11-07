a69d958e19560b77a64808d260e5264aceebd3ab
Alfredo <rosendo.mescco@tecsup.edu.pe>
2023-11-07 17:05:23 -0500

    Initial Commit

---
+++ MesccoSnapchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MesccoSnapchat
+//
+//  Created by Mescco Quiespe, Rosendo Alfredo on 7/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
