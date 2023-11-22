b02ac5883cb3059f68b712e034a4897fa1c3323f
Alfredo <rosendo.mescco@tecsup.edu.pe>
2023-11-21 19:02:39 -0500

    lab13_completo

---
+++ MesccoSnapchat/Modelos/Snap.swift
@@ -0,0 +1,16 @@
+//
+//  Snap.swift
+//  MesccoSnapchat
+//
+//  Created by Mescco Quiespe, Rosendo Alfredo on 21/11/23.
+//
+
+import Foundation
+
+class Snap {
+    var imagenURL = ""
+    var descrip = ""
+    var from = ""
+    var id = ""
+    var imagenID = ""
+}
---
+++ MesccoSnapchat/Modelos/Usuario.swift
@@ -0,0 +1,13 @@
+//
+//  Usuario.swift
+//  MesccoSnapchat
+//
+//  Created by Mescco Quiespe, Rosendo Alfredo on 21/11/23.
+//
+
+import Foundation
+
+class Usuario {
+    var email = ""
+    var uid = ""
+}
